3cb91b2568663e0b8db0b6151d9b8e05a138ff86
Malhar Jadhav <malharjadhav8999@gmail.com>
2023-06-01 16:21:15 +0530

    added cors

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+/**
+    @Author - Malhar
+    @description - Node assignments
+*/
+
+
 // Assignment 1 - Hello World
 
 app = require('express');
@@ -11,10 +17,21 @@ express.listen(3000, () => {
     console.log('Port 3000 is here......')
 })
 
-
-// Assignment 2 - Passing port and env from command line to node server
+// Assignment 2 - pass port and env to node server from command line arguments and set those values in server
 
 console.log('argv', process.argv);
 const args = process.argv.splice(2);
 const env = args[0].split('=')[1];    // env=dev
 const port = args[1].split('=')[1]    // port=3000
+
+
+// Assignment 3 - Add CORS to your nodejs app and integrate mysql
+
+// adding cors to system
+var cors = require('cors');
+app.use(cors());
+
+
+express.listen(port, () => {
+    console.log(`Port ${port} is here......`)
+})
